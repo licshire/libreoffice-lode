ae38b2942bef6f49eebce43c7d7404f7cd3dec2e
Norbert Thiebaud <nthiebaud@gmail.com>
2014-10-15 15:03:15 -0500

    cygwin typo in variable that point to the make.exe's name downloaded

--- bin/utils_Cygwin.sh
+++ bin/utils_Cygwin.sh
@@ -3,12 +3,12 @@
 
 install_pre_build_native_make()
 {
-    local make_exe_file=make-85047eb-msvs.exe
+    local make_exe_file="make-85047eb-msvs.exe"
 
-    if [ ! -f "${BASE_DIR?}/packages/${make_exec_file?}" -o ! -x "${BASE_DIR?}/opt/bin/make" ] ; then
+    if [ ! -f "${BASE_DIR?}/packages/${make_exe_file?}" -o ! -x "${BASE_DIR?}/opt/bin/make" ] ; then
         pushd "${BASE_DIR?}/packages" > /dev/null || die "Error switching to ${BASE_DIR?}/packages"
-        rm -fr "${make_exec_file?}"
-        wget -O "http://dev-www.libreoffice.org/bin/cygwin/${make_exec_file?}" || die "Error download ${module?} source package"
+        rm -fr "${make_exe_file?}"
+        wget -O "http://dev-www.libreoffice.org/bin/cygwin/${make_exe_file?}" || die "Error download ${module?} source package"
         cp "${make_exe_file?}" "${BASE_DIR?}/opt/bin/make" || die "Error copying make"
         chmod a+x "${BASE_DIR?}/opt/bin/make" || die "Error chmoding installed make"
         popd > /dev/null || die "Error poping ${BASE_DIR?}/packages"
