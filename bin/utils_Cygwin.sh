27396b95bb35a56a7ecd4ddf6ad2e5d6c9697026
Norbert Thiebaud <nthiebaud@gmail.com>
2014-10-15 15:13:49 -0500

    cygwin: pre-create opt/bin directory to install make

--- bin/utils_Cygwin.sh
+++ bin/utils_Cygwin.sh
@@ -6,6 +6,7 @@ install_pre_build_native_make()
     local make_exe_file="make-85047eb-msvc.exe"
 
     if [ ! -f "${BASE_DIR?}/packages/${make_exe_file?}" -o ! -x "${BASE_DIR?}/opt/bin/make" ] ; then
+        test_create_dirs "${BASE_DIR?}/opt/bin"
         pushd "${BASE_DIR?}/packages" > /dev/null || die "Error switching to ${BASE_DIR?}/packages"
         rm -fr "${make_exe_file?}"
         wget "http://dev-www.libreoffice.org/bin/cygwin/${make_exe_file?}" || die "Error download ${module?} source package"
