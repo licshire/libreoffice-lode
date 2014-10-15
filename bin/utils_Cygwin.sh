dd70cdf018204d8e438ee13d2e50376ca38cefd5
Norbert Thiebaud <nthiebaud@gmail.com>
2014-10-15 15:04:33 -0500

    cygwin error in wget command to download make.exe

--- bin/utils_Cygwin.sh
+++ bin/utils_Cygwin.sh
@@ -8,7 +8,7 @@ install_pre_build_native_make()
     if [ ! -f "${BASE_DIR?}/packages/${make_exe_file?}" -o ! -x "${BASE_DIR?}/opt/bin/make" ] ; then
         pushd "${BASE_DIR?}/packages" > /dev/null || die "Error switching to ${BASE_DIR?}/packages"
         rm -fr "${make_exe_file?}"
-        wget -O "http://dev-www.libreoffice.org/bin/cygwin/${make_exe_file?}" || die "Error download ${module?} source package"
+        wget "http://dev-www.libreoffice.org/bin/cygwin/${make_exe_file?}" || die "Error download ${module?} source package"
         cp "${make_exe_file?}" "${BASE_DIR?}/opt/bin/make" || die "Error copying make"
         chmod a+x "${BASE_DIR?}/opt/bin/make" || die "Error chmoding installed make"
         popd > /dev/null || die "Error poping ${BASE_DIR?}/packages"
