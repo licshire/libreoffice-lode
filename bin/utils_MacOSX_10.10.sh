21aaa7de23a14b5345993dfc39e47a7f4008c619
Norbert Thiebaud <nthiebaud@gmail.com>
2015-04-03 18:11:05 -0500

    lode: update doxygen on macosx 10.10

--- bin/utils_MacOSX_10.10.sh
+++ bin/utils_MacOSX_10.10.sh
@@ -17,7 +17,7 @@ install_build_dep()
     install_generic_conf_make_install "make" "4.1" "http://mirrors.kernel.org/gnu/make" "make-4.1.tar.gz"
     install_ant
     install_junit
-    install_generic_conf_make_install "doxygen" "1.8.8" "http://ftp.stack.nl/pub/users/dimitri" "doxygen-1.8.8.src.tar.gz"
+    install_generic_conf_make_install "doxygen" "1.8.9.1" "http://ftp.stack.nl/pub/users/dimitri" "doxygen-1.8.9.1.src.tar.gz"
     install_generic_conf_make_install "ccache" "3.1.9" "https://www.samba.org/ftp/ccache" "ccache-3.1.9.tar.gz"
     install_default_autogen_input
 }
