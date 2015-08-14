35eaf7cf3f99ca1fc6b05944d57bcc56e6c574e2
Norbert Thiebaud <nthiebaud@gmail.com>
2015-08-13 22:18:56 -0500

    typo in SetAutoLogon

--- bin/SetAutoLogonRegistry.ps1
+++ bin/SetAutoLogonRegistry.ps1
@@ -9,7 +9,7 @@ if( $user = '' )
 $password = Read-Host 'What is the password?'
 
 $RegPath = "HKLM\Software\Microsoft\Windows NT\CurrentVersion\winlogon"
-Set-ItemProperty $RegPAth "AutoAdminLogon" -Value "1" -type String
-Set-ItemProperty $RegPAth "DefaultUsername" -Value "$user" -type String
-Set-ItemProperty $RegPAth "DefaultPassword" -Value "$password" -type String
+Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String
+Set-ItemProperty $RegPath "DefaultUsername" -Value "$user" -type String
+Set-ItemProperty $RegPath "DefaultPassword" -Value "$password" -type String
 
