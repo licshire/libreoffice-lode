fb70a4c190bac6f6de5fe85e997e7545d3149a23
Norbert Thiebaud <nthiebaud@gmail.com>
2015-08-25 10:59:26 -0500

    upgrade doxygen for Mac OSX 10.10

--- bin/utils_MacOSX_10.10.sh
+++ bin/utils_MacOSX_10.10.sh
@@ -17,7 +17,8 @@ install_build_dep()
     install_generic_conf_make_install "make" "4.1" "http://mirrors.kernel.org/gnu/make" "make-4.1.tar.gz"
     install_ant
     install_junit
-    install_generic_conf_make_install "doxygen" "1.8.9.1" "http://ftp.stack.nl/pub/users/dimitri" "doxygen-1.8.9.1.src.tar.gz"
+    install_private_cmake "3.3.1" "http://www.cmake.org/files/v3.3/" "cmake-3.3.1.tar.gz"
+    install_doxygen "1.8.10" "http://ftp.stack.nl/pub/users/dimitri" "doxygen-1.8.10.src.tar.gz"
     install_generic_conf_make_install "ccache" "3.1.9" "https://www.samba.org/ftp/ccache" "ccache-3.1.9.tar.gz"
     install_default_autogen_input
 }
