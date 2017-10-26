712f00e180b94b225a1a01e1233a7007621d8472
Bartosz Kosiorek <gang65@poczta.onet.pl>
2017-10-26 15:56:07 +0200

    Add support for Xcode 9 on all macOS versions
    During Cmake compilation with Xcode 9, the error that
    two missing symbols are missing, is displayed:
      utimensat and futimens
    The symbols are not needed, and the issue was fixed
    in CMake 3.10:
    https://gitlab.kitware.com/cmake/cmake/issues/17101
    
    To allow compiling LibreOffice with Xcode 9,
    we need to bump cmake version to 3.10
    Change-Id: I8fd9a170baceabda4ed35d82657642b4858d4ffa
    Reviewed-on: https://gerrit.libreoffice.org/43900
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- bin/utils_MacOSX_10.11.sh
+++ bin/utils_MacOSX_10.11.sh
@@ -12,7 +12,9 @@ install_build_dep()
     install_generic_conf_make_install "make" "4.1" "http://mirrors.kernel.org/gnu/make" "make-4.1.tar.gz"
     install_ant
     install_junit
-    install_private_cmake "3.3.1" "http://www.cmake.org/files/v3.3/" "cmake-3.3.1.tar.gz"
+    # To resolve issue with Xcode 9, we need to use Cmake 3.10:
+    # https://gitlab.kitware.com/cmake/cmake/issues/17101
+    install_private_cmake "3.10.0-rc3" "http://www.cmake.org/files/v3.10/" "cmake-3.10.0-rc3.tar.gz"
     install_doxygen "1.8.10" "http://ftp.stack.nl/pub/users/dimitri" "doxygen-1.8.10.src.tar.gz"
     install_generic_conf_make_install "ccache" "3.1.9" "https://www.samba.org/ftp/ccache" "ccache-3.1.9.tar.gz"
     install_generic_conf_make_install "gettext" "0.19.8" "http://ftp.gnu.org/pub/gnu/gettext" "gettext-0.19.8.tar.gz"
